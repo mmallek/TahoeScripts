7c03487c95476c54559c03a30ec59c9ffdad0b3b
Maritza Mallek <mmallek@eco.umass.edu>
2013-05-29 10:37:59 -0400

    Commit of existing succession transition functions.
    Would be good to update core script to better handle changed YHR types, but for now the method is to save a new script for each YHR type.
    
     Initial commit
    	new file:   SuccessionTransitionFunctionMHW.py
    	new file:   SuccessionTransitionFunctionOCFW.py
    	new file:   SuccessionTransitionFunctionRFR.py
    	new file:   SuccessionTransitionFunctionSMC.py

---
+++ SuccessionTransitionFunctionOCFW.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ed_trans_prob(*args):
+
+     # Input Variables:
+    # PS = prob_succession : probability of succession per timestep
+    # SL = stage_length : maximum time spent in stage :
+    #      equal to "after X years" (consider changing text to "by X years"
+    # SY = start_year : year in stage at which probability of succession
+    #      is implemented : subtract 10 from "begin transition after X years"
+    tau = 10
+    # AS : age range during which succession can take place
+    for label, PS, SY, SL in args:
+        AS = np.arange(0, SL + 3*tau, tau)
+        if PS < 1:
+           transition_prob = (1 - PS) ** ((AS - SY) / tau)
+        elif PS == 1:
+            transition_prob = np.zeros_like(AS)
+        transition_prob[AS <= SY] = 1
+        transition_prob[AS >= SL] = 0
+        plt.plot(AS, transition_prob, label=label)
+    plt.title("Transition Probability for OCFW, Early Development")
+    plt.xlabel("Years in Stage")
+    plt.ylabel("Probability of patch remaining in Early Development")
+    plt.legend()
+
+def mdc_trans_prob(*args):
+     # Input Variables:
+    # PS = prob_succession : probability of succession per timestep
+    # SL = stage_length : maximum time spent in stage :
+    #      equal to "after X years" (consider changing text to "by X years"
+    # SY = start_year : year in stage at which probability of succession
+    #      is implemented : subtract 10 from "begin transition after X years"
+    tau = 10
+    # AS : age range during which succession can take place
+    for label, PS, SY, SL in args:
+        AS = np.arange(0, SL + 3*tau, tau)
+        if PS < 1:
+           transition_prob = (1 - PS) ** ((AS - SY) / tau)
+        elif PS == 1:
+            transition_prob = np.zeros_like(AS)
+        transition_prob[AS <= SY] = 1
+        transition_prob[AS >= SL] = 0
+        plt.plot(AS, transition_prob, label=label)
+    plt.title("Transition Probability for OCFW, Mid Development Closed")
+    plt.xlabel("Years in Stage")
+    plt.ylabel("Probability of patch remaining in Mid Development Closed")
+    plt.legend()
+
+def mdo_trans_prob(*args):
+
+     # Input Variables:
+    # PS = prob_succession : probability of succession per timestep
+    # SL = stage_length : maximum time spent in stage :
+    #      equal to "after X years" (consider changing text to "by X years"  
+    # SY = start_year : year in stage at which probability of succession
+    #      is implemented : subtract 10 from "begin transition after X years"
+    tau = 10
+    # AS : age range during which succession can take place
+    for label, PS, SY, SL in args:
+        AS = np.arange(0, SL + 3*tau, tau)
+        if PS < 1:
+           transition_prob = (1 - PS) ** ((AS - SY) / tau)
+        elif PS == 1:
+            transition_prob = np.zeros_like(AS)
+        transition_prob[AS <= SY] = 1
+        transition_prob[AS >= SL] = 0
+        plt.plot(AS, transition_prob, label=label)
+    plt.title("Transition Probability for OCFW, Mid Development Open")
+    plt.xlabel("Years in Stage")
+    plt.ylabel("Probability of patch remaining in Mid Development Open")
+    plt.legend()
+
+
+def lda_trans_prob(*args):
+
+     # Input Variables:
+    # PS = prob_succession : probability of succession per timestep
+    # SL = stage_length : maximum time spent in stage :
+    #      equal to "after X years" (consider changing text to "by X years"  
+    # SY = start_year : year in stage at which probability of succession
+    #      is implemented : subtract 10 from "begin transition after X years"
+    tau = 10
+    # AS : age range during which succession can take place
+    for label, PS, SY, SL in args:
+        AS = np.arange(0, SL + 3*tau, tau)
+        if PS < 1:
+           transition_prob = (1 - PS) ** ((AS - SY) / tau)
+        elif PS == 1:
+            transition_prob = np.zeros_like(AS)
+        transition_prob[AS <= SY] = 1
+        transition_prob[AS >= SL] = 0
+        plt.plot(AS, transition_prob, label=label)
+    plt.title("Transition Probability for OCFW, Late Development")
+    plt.xlabel("Years in Stage")
+    plt.ylabel("Probability of patch remaining in Late Development")
+    plt.legend()
